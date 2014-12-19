91177d88035f3e069e78b64d1779bc5aaf26280c
Musienko Viktor <treasury23@gmail.com>
2014-12-19 12:10:10 +0200

    add button remove,checkbox

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 /**
  * Created by vitya on 19.12.14.
  */
+
+var input = document.querySelector('#input-text'),
+    ul = document.querySelector('#list').appendChild(document.createElement('ul'));
+
+input.addEventListener('keyup', function(e){
+
+    if (e.target.value != '' && e.keyCode === 13) {
+
+        var li = document.createElement('li'),
+            button = document.createElement('button');
+
+        button.className='button';
+        button.innerHTML = 'Remove'
+
+        li.innerHTML = '<input type="checkbox">' + e.target.value;
+
+        ul.appendChild(li).appendChild(button);
+        input.value = "";
+    }
+})
